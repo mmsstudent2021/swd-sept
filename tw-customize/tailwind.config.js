b5259294f74c01fe6ce01a5ea1e8a72a8522a259
mmsstudent2021 <mmsstudent2021@gmail.com>
2024-10-31 20:42:19 +0630

    daisy ui

--- tw-customize/tailwind.config.js
+++ tw-customize/tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
   content: [
     "./src/**/*.{html,js}",
     "./node_modules/flowbite/**/*.js"
-  
   ],
   theme: {
     extend: {
@@ -11,17 +10,54 @@ module.exports = {
         'text': '#030214',
         'background': '#f7f7fe',
         'primary': '#2e22e4',
-        'secondary': '#dd7fef',
-        'accent': '#e94ed6',
+        'secondary': '#dd7fef', // Comma added here
+        'accent': '#e94ed6'
       },
       fontFamily : {
-        'noto-mm' : ["Noto Serif Myanmar",'san-serif'],
-        'poppins' :[ "Poppins",'san-serif']
+        'noto-mm' : ["Noto Serif Myanmar",'sans-serif'],
+        'poppins' :[ "Poppins",'sans-serif'],
+        'noto-poppins' : ["Poppins","Noto Serif Myanmar", 'sans-serif']
       }
     },
   },
   plugins: [
-    require('flowbite/plugin')
+    require('daisyui'),
   ],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+    ],
+  },
+  
 }
-
