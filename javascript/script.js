9b83492a29ccc68e966fba8079e3df2f54cd7cf3
mmsstudent2021 <mmsstudent2021@gmail.com>
2024-11-13 20:53:37 +0630

    js

---
+++ javascript/script.js
@@ -0,0 +1,41 @@
+
+
+let x = 20;
+console.log(x);
+
+ x = 10;
+
+ console.log(10);
+ 
+ const y = 10;
+ console.log(y);
+
+ let myName = "Kyaw Kyaw";
+ console.log(typeof myName);
+
+ let myAge = 15;
+ console.log(typeof myAge);
+ 
+ let isDrink = false;
+ console.log(typeof isDrink);
+
+ let myFavFood = ["Orange", "Apple", "Grape", "Mango", null, undefined]
+ console.log( myFavFood);
+
+ let mySelf = {
+    name : "Kyaw Kyaw",
+    job : "Programmer"
+ }
+
+ console.log(mySelf.name);
+
+
+ 
+
+
+ 
+ 
+ 
+
+ 
+
